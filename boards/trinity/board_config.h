d0574e39b89dc7ec1e425af10b1416eb29f456fa
Gustav Karlsson <gkarlsson@karlssonrobotics.com>
2021-08-10 14:26:52 -0400

    added trinity board and timeout

---
+++ boards/trinity/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Karlsson Robotics"
+#define PRODUCT_NAME "Trinity"
+#define VOLUME_LABEL "TRINITY"
+#define INDEX_URL "http://kr4.us"
+#define BOARD_ID "SAMD21G18A-Trinity-v1"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA14
+#define LED_TX_PIN PIN_PA17
+#define LED_RX_PIN PIN_PA16
+
+#endif
